3e2966a6b7b438a5ac5ecf7febd3bf194fdcfef3
JACQUES\jacob <jtdehaan@indiana.edu>
2017-10-24 14:07:27 -0400

    basic methods theory

--- Minesweeper/src/application/Board.java
+++ Minesweeper/src/application/Board.java
@@ -5,6 +5,29 @@ public class Board {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}/*
+	public void reveal(cell a) {
+		//recursive
+		if(Cell.isMine(a)==true) {
+			GUI.end();
+		}else if(Cell.getCount(a)==0){
+			reveal(cells around);
+		}else {
+			a.setText(Cell.getCount());
+		}
 	}
-
+	public void mark(cell a) {
+		
+	}
+	public void unmask() {
+		for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+            	if(cell.isMine(a)==true) {
+            		a.setText();
+            	}else if(cell.getCount(a)) {
+            		
+            	}
+            }
+		}
+	}*/
 }
