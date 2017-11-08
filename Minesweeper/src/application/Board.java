77243c084288f8e5f769bdf2266922f88f66537e
JACQUES\jacob <jtdehaan@indiana.edu>
2017-11-08 16:10:05 -0500

    added mine creation and count creation

--- Minesweeper/src/application/Board.java
+++ Minesweeper/src/application/Board.java
@@ -5,14 +5,27 @@ public class Board {
 	public static void main(String[] args) {
 		
 	}
+	boolean win=false;
+	boolean loss =false;
+	//returns win state
+	public boolean win(){
+		return win;
+	}
+	//returns loss state
+	public boolean loss() {
+		return loss;
+	}
+	//creates array
 	cell[][] cellArray= new cell[10][10];
 	cell test = new cell();
+	//setsup cellArray
 	public void setup(){
 		cell cellArray[][]={};
 		for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
             	cellArray[i][j]=test;
             }}
+		//creates  mines
 		for(int i =0;i<10;i++) {
 			int x,y;
 			x=ThreadLocalRandom.current().nextInt(0, 10 + 1);
@@ -21,10 +34,19 @@ public class Board {
 				i--;
 			}else {
 				cellArray[x][y].setMine(true);
+				//set values of around mines +1
+				cellArray[x+1][y+1].setAroundMines(cellArray[x+1][y+1].getAroundMines()+1);
+				cellArray[x+1][y].setAroundMines(cellArray[x+1][y].getAroundMines()+1);
+				cellArray[x][y+1].setAroundMines(cellArray[x][y+1].getAroundMines()+1);
+				cellArray[x][y].setAroundMines(cellArray[x][y].getAroundMines()+1);
+				cellArray[x][y-1].setAroundMines(cellArray[x][y-1].getAroundMines()+1);
+				cellArray[x-1][y].setAroundMines(cellArray[x-1][y].getAroundMines()+1);
+				cellArray[x-1][y-1].setAroundMines(cellArray[x-1][y-1].getAroundMines()+1);
+				
 			}
 		}
 	}
-	
+	//reveals the cell
 	public void reveal(int i, int j) {
 		//recursive
 		if (cellArray[i][j].isCovered()==true){
@@ -40,7 +62,10 @@ public class Board {
 			reveal(i-1,j+1);
 			reveal(i-1,j);
 			reveal(i-1,j-1);
+		}else if(cellArray[i][j].returnV()=="B") {
+			loss=true;
 		}
+		checkWin();
 		
 	}
 		
@@ -63,14 +88,14 @@ public class Board {
 		}
 	}
 	
-	public boolean checkWin(){
+	public void checkWin(){
 		for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
             if(cellArray[i][j].isMine()==true || cellArray[i][j].isCovered()==true){
             }else{
-            	return false;
+            	win =false;
             }}
          }
-         return true;
+         win =true;
 	}
 }
